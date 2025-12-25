08e8940f9d90634ef06278121ab0f45477750567
Joao <froes544@gmail.com>
2025-12-25 16:14:39 -0300

    init: Inicializado o projeto com as dependencias necessárias

---
+++ src/main/java/com/joaofroes/gestao_pedidos/GestaoDePedidosApplication.java
@@ -0,0 +1,13 @@
+package com.joaofroes.gestao_pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDePedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDePedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaofroes/gestao_pedidos/GestaoDePedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaofroes.gestao_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDePedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
